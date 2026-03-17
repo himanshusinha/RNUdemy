c6044062a20ed62cb28510c746c979d76b99aa59
hsinha1110 <himanshusinha1110@gmail.com>
2026-03-17 20:55:52 +0530

    styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.dummyText}>
+        <Text>Another piece of text!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!</Text>
+      <Button title="Tap new!" />
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  dummyText: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: "blue",
+  },
+});
